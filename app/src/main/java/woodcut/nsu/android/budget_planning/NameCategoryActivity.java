932263d2905455e82c46736b526f94d42ff04f6e
Anastasiya Zykova <azalekss@mail.ru>
2019-02-03 22:42:11 +0700

    Initial commit

---
+++ app/src/main/java/woodcut/nsu/android/budget_planning/NameCategoryActivity.java
@@ -0,0 +1,13 @@
+package woodcut.nsu.android.budget_planning;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NameCategoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_cat_name);
+    }
+
+}
